b2d229ad43eb8eb4ee56b6b2b70b1a508c315df5
Prabhat020 <kumar.prabhat23343@jeckukas.org.in>
2025-09-06 18:28:29 +0530

    dist is added

---
+++ dist/Grocery.41713dbe.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire59d3;(0,e.register)("agtsM",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),t=()=>(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"This is the grocery which will contain more and more component"})})});
+//# sourceMappingURL=Grocery.41713dbe.js.map
